6308417558e65da1eb67f6e3050fa880b71235e6
Adauto Neto <stanleystl@gmail.com>
2013-09-25 12:14:00 -0300

    Initial commit

---
+++ ActiveDirectoryHelper.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices;
+using System.Configuration;
+
+namespace ActiveDirectory
+{
+    public class ActiveDirectoryHelper
+    {
+        private string _customPath;
+        
+        private DirectoryEntry _directoryEntry = null;
+
+        private DirectoryEntry SearchRoot
+        {
+            get
+            {
+                if (_directoryEntry == null)
+                {
+                    _directoryEntry = new DirectoryEntry(LDAPPath, LDAPUser, LDAPPassword, AuthenticationTypes.Secure);
+                }
+                return _directoryEntry;
+            }
+        }
+
+        private String LDAPPath
+        {
+            get
+            {
+                return _customPath ?? ConfigurationManager.AppSettings["LDAPPath"];
+            }
+        }
+
+        private String LDAPUser
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["LDAPUser"];
+            }
+        }
+
+        private String LDAPPassword
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["LDAPPassword"];
+            }
+        }
+
+        private String LDAPDomain
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["LDAPDomain"];
+            }
+        }
+
+        public ActiveDirectoryHelper() { }
+
+        public ActiveDirectoryHelper(string path)
+        {
+            _customPath = path;
+        }
+
+        internal ADUserDetail GetUserByFullName(String userName)
+        {
+            try
+            {
+                _directoryEntry = null;
+                DirectorySearcher directorySearch = new DirectorySearcher(SearchRoot);
+                directorySearch.Filter = "(&(objectClass=user)(cn=" + userName + "))";
+                SearchResult results = directorySearch.FindOne();
+
+                if (results != null)
+                {
+                    DirectoryEntry user = new DirectoryEntry(results.Path, LDAPUser, LDAPPassword);
+                    return ADUserDetail.GetUser(user);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public List<ADUserDetail> GetAllUsers()
+        {
+            List<ADUserDetail> list = new List<ADUserDetail>();
+            DirectorySearcher directorySearcher = new DirectorySearcher(SearchRoot);
+            directorySearcher.Filter = @"(&(objectCategory=User)(!EmployeeId=Services))";
+            SearchResultCollection results = directorySearcher.FindAll();
+
+            if (results != null)
+            {
+                foreach (SearchResult result in results)
+                {
+                    DirectoryEntry user = new DirectoryEntry(result.Path, LDAPUser, LDAPPassword);
+                    list.Add(ADUserDetail.GetUser(user));
+                }
+            }
+            return list;
+        }
+
+        public ADUserDetail GetUserByLoginName(String userName)
+        {
+            try
+            {
+                _directoryEntry = null;
+                DirectorySearcher directorySearch = new DirectorySearcher(SearchRoot);
+                directorySearch.Filter = "(&(objectClass=user)(SAMAccountName=" + userName + "))";
+                SearchResult results = directorySearch.FindOne();
+
+                if (results != null)
+                {
+                    DirectoryEntry user = new DirectoryEntry(results.Path, LDAPUser, LDAPPassword);
+                    return ADUserDetail.GetUser(user);
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// This function will take a DL or Group name and return list of users
+        /// </summary>
+        /// <param name="groupName"></param>
+        /// <returns></returns>
+        public List<ADUserDetail> GetUserFromGroup(String groupName)
+        {
+            List<ADUserDetail> userlist = new List<ADUserDetail>();
+            try
+            {
+                _directoryEntry = null;
+                DirectorySearcher directorySearch = new DirectorySearcher(SearchRoot);
+                directorySearch.Filter = "(&(objectClass=group)(SAMAccountName=" + groupName + "))";
+                SearchResult results = directorySearch.FindOne();
+                if (results != null)
+                {
+
+                    DirectoryEntry deGroup = new DirectoryEntry(results.Path, LDAPUser, LDAPPassword);
+                    System.DirectoryServices.PropertyCollection pColl = deGroup.Properties;
+                    int count = pColl["member"].Count;
+
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        string respath = results.Path;
+                        string[] pathnavigate = respath.Split("CN".ToCharArray());
+                        respath = pathnavigate[0];
+                        string objpath = pColl["member"][i].ToString();
+                        string path = respath + objpath;
+
+
+                        DirectoryEntry user = new DirectoryEntry(path, LDAPUser, LDAPPassword);
+                        ADUserDetail userobj = ADUserDetail.GetUser(user);
+                        userlist.Add(userobj);
+                        user.Close();
+                    }
+                }
+                return userlist;
+            }
+            catch (Exception ex)
+            {
+                return userlist;
+            }
+
+        }
+
+        #region Get user with First Name
+
+        public List<ADUserDetail> GetUsersByFirstName(string fName)
+        {
+
+            //UserProfile user;
+            List<ADUserDetail> userlist = new List<ADUserDetail>();
+            string filter = "";
+
+            _directoryEntry = null;
+            DirectorySearcher directorySearch = new DirectorySearcher(SearchRoot);
+            directorySearch.Asynchronous = true;
+            directorySearch.CacheResults = true;
+            filter = string.Format("(givenName={0}*", fName);
+            //            filter = "(&(objectClass=user)(objectCategory=person)(givenName="+fName+ "*))";
+
+
+            directorySearch.Filter = filter;
+
+            SearchResultCollection userCollection = directorySearch.FindAll();
+            foreach (SearchResult users in userCollection)
+            {
+                DirectoryEntry userEntry = new DirectoryEntry(users.Path, LDAPUser, LDAPPassword);
+                ADUserDetail userInfo = ADUserDetail.GetUser(userEntry);
+
+                userlist.Add(userInfo);
+
+            }
+
+            directorySearch.Filter = "(&(objectClass=group)(SAMAccountName=" + fName + "*))";
+            SearchResultCollection results = directorySearch.FindAll();
+            if (results != null)
+            {
+
+                foreach (SearchResult r in results)
+                {
+                    DirectoryEntry deGroup = new DirectoryEntry(r.Path, LDAPUser, LDAPPassword);
+
+                    ADUserDetail agroup = ADUserDetail.GetUser(deGroup);
+                    userlist.Add(agroup);
+                }
+
+            }
+            return userlist;
+        }
+
+        #endregion
+
+
+        #region AddUserToGroup
+        public bool AddUserToGroup(string userlogin, string groupName)
+        {
+            try
+            {
+                _directoryEntry = null;
+                ADManager admanager = new ADManager(LDAPDomain, LDAPUser, LDAPPassword);
+                admanager.AddUserToGroup(userlogin, groupName);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region RemoveUserToGroup
+        public bool RemoveUserToGroup(string userlogin, string groupName)
+        {
+            try
+            {
+                _directoryEntry = null;
+                ADManager admanager = new ADManager("xxx", LDAPUser, LDAPPassword);
+                admanager.RemoveUserFromGroup(userlogin, groupName);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
